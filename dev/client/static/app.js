45026d1316072ba34c2706d9d9a8e4b98f8a0879
Hedy <81718073+Hedy-dev@users.noreply.github.com>
2024-01-29 00:42:25 +0300

    static updated
    click markers function add

--- dev/client/static/app.js
+++ dev/client/static/app.js
@@ -3,11 +3,14 @@ const App = {
         return {
             // counter: 0,
             currentPage: 0,
-            addPlace: 1,
-            addRoute :0,
+            addPlace: 0,
+            addRoute :1,
             placeholderString : "введите название заметок",
             title: "список заметок",
             inputValue: "",
+
+
+
             newsListLeft : [
                 {
                     type : "event",
@@ -70,7 +73,15 @@ const App = {
                 }
             ],
 
+            editingRouteList : [
+
+            ],
+
+            selectedOnRouteEditMapSight : '',
+
             notes : ['task 1', 'task 2'],
+
+            
             
         }
     },
@@ -87,7 +98,14 @@ const App = {
             this.startMap();
         }
         if (this.currentPage == 3) {
+            if (this.addPlace) {
             this.startMap();
+
+            }
+            if (this.addRoute) {
+                this.startCreatingNewRouteMap();
+            }
+            
         }
         if (this.currentPage == 4) {
             
@@ -138,53 +156,197 @@ const App = {
             // map.addLayer(markers);
             // markers.addMarker(new OpenLayers.Marker(position));
             var markers = new OpenLayers.Layer.Markers("Markers");
-
-            listd = [//30.3648853,59.9306793
-                {Longitude : 30.3648853,
-                    Latitude : 59.9306793   ,
-                    id : 0,
-                }, //30.4102421,59.9239807
-                {Longitude : 30.4102421,
-                    Latitude : 59.9239807,
-                    id : 1,
-                },
-            ]
-            console.log(listd.length);
-            for(var i = 0; i < listd.length; i++)
-            { 
-                (function(i){
-                     var lonLat = new OpenLayers.LonLat(listd[i].Longitude, listd[i].Latitude).transform( fromProjection, toProjection);
-                    console.log(lonLat);
-                    
-                    //  var title = listd[i].Title;
-                    //  var iconPath = listd[i].IconPath;
-                    //  var size = new OpenLayers.Size(15, 22);
-                    //  var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
             
-                    //  var icon = new OpenLayers.Icon(iconPath, size, offset);
-                     var marker = new OpenLayers.Marker(lonLat); //, icon.clone()
-                    marker.id = listd[i].id
-                    // console.log(marker);
-                     markers.addMarker(marker);
-                    //  console.log(markers);
-                    
-                    marker.events.register("click", marker, function(e){
-                    //     // popup = new OpenLayers.Popup.FramedCloud("chicken",
-                    //     //     marker.lonlat,
-                    //     //     new OpenLayers.Size(200, 200),
-                    //     //     title,
-                    //     //     null, false );
-                    //     //   map.addPopup(popup);
-                        console.log(marker.id);
-                     });
-                })(i);
-             } 
-             map.addLayer(markers);
-            //  console.log(map);
+            function updateSightsPoints(listd) {
+                // window.map.getLayers().getArray()
+                // .filter(layer => layer.get('name') === 'Markers')
+                // .forEach(layer => map.removeLayer(layer));
 
+                markers.clearMarkers();
+                
+                console.log(listd.length);
+                for(var i = 0; i < listd.length; i++)
+                { 
+                    (function(i){
+                        var lonLat = new OpenLayers.LonLat(listd[i].lon, listd[i].lat).transform( fromProjection, toProjection);
+                        console.log(lonLat);
+                        
+                        //  var title = listd[i].Title;
+                        //  var iconPath = listd[i].IconPath;
+                        //  var size = new OpenLayers.Size(15, 22);
+                        //  var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+                
+                        //  var icon = new OpenLayers.Icon(iconPath, size, offset);
+                        var marker = new OpenLayers.Marker(lonLat); //, icon.clone()
+                        marker.id = listd[i].id
+                        // console.log(marker);
+                        markers.addMarker(marker);
+                        //  console.log(markers);
+                        
+                        marker.events.register("click", marker, function(e){
+                        //     // popup = new OpenLayers.Popup.FramedCloud("chicken",
+                        //     //     marker.lonlat,
+                        //     //     new OpenLayers.Size(200, 200),
+                        //     //     title,
+                        //     //     null, false );
+                        //     //   map.addPopup(popup);
+                        
+                        
+                            console.log(marker.id);
+                        });
+                    })(i);
+                } 
+                
+
+            }
+            //  console.log(map);
+            map.addLayer(markers);
+
+            
 
             map.setCenter(position, zoom);
-            console.log("getExtent = ", map.getExtent().transform(toProjection, fromProjection));
+
+            console.log(map);
+            map.events.register("moveend", map, function() {
+                getSightsPoins(map);
+                
+            });
+
+            getSightsPoins = (map) => {
+                let extent = map.getExtent().transform(toProjection, fromProjection);
+                console.log("getExtent = ", extent);
+
+                fetch("/get/dysplayable/points", 
+                {
+                    method: "POST",
+                    body: JSON.stringify({ 
+                        extent : extent
+                    }),
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                        // Authorization: 'Bearer '+ localStorage.access_token ,
+                    }
+                    }
+                    ).then((response) => response.json()).then((json) => {
+                        // console.log('json[sights] = ', json['sights']);
+                        // console.log('json[sights] = ',  JSON.parse(json)['sights']);
+
+                        updateSightsPoints(JSON.parse(json)['sights']);
+                      
+                    });
+                }
+
+                getSightsPoins(map);
+            
+        },
+
+        startCreatingNewRouteMap() {
+            console.log("map called", document.getElementById("demoMap"));
+            
+            
+            var lat            = 59.9275;
+            var lon            = 30.3346;
+            var zoom           = 12;
+
+            var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+            var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+            var position       = new OpenLayers.LonLat(lon, lat).transform( fromProjection, toProjection);
+
+            map = new OpenLayers.Map("demoMap");
+            var mapnik         = new OpenLayers.Layer.OSM();
+            map.addLayer(mapnik);
+
+            
+            var markers = new OpenLayers.Layer.Markers("Markers");
+            
+            updateSightsPoints = (listd) => {
+               
+                markers.clearMarkers();
+                
+                console.log(listd.length);
+                for(var i = 0; i < listd.length; i++)
+                { 
+                    ((i)=> {
+                        var lonLat = new OpenLayers.LonLat(listd[i].lon, listd[i].lat).transform( fromProjection, toProjection);
+                        console.log(lonLat);
+                        
+                        //  var title = listd[i].Title;
+                        //  var iconPath = listd[i].IconPath;
+                        //  var size = new OpenLayers.Size(15, 22);
+                        //  var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+                
+                        //  var icon = new OpenLayers.Icon(iconPath, size, offset);
+                        var marker = new OpenLayers.Marker(lonLat); //, icon.clone()
+                        marker.id = listd[i].id
+                        markers.addMarker(marker);
+                        
+                        marker.events.register("click", marker, (e)=>{
+                            this.getSightById(marker.id)
+                        });
+                    })(i);
+                } 
+                
+
+            }
+            //  console.log(map);
+            map.addLayer(markers);
+
+            
+
+            map.setCenter(position, zoom);
+
+            console.log(map);
+            map.events.register("moveend", map, function() {
+                getSightsPoins(map);
+                
+            });
+
+            getSightsPoins = (map) => {
+                let extent = map.getExtent().transform(toProjection, fromProjection);
+                console.log("getExtent = ", extent);
+
+                fetch("/get/dysplayable/points", 
+                {
+                    method: "POST",
+                    body: JSON.stringify({ 
+                        extent : extent
+                    }),
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                        // Authorization: 'Bearer '+ localStorage.access_token ,
+                    }
+                    }
+                    ).then((response) => response.json()).then((json) => {
+                       
+                        updateSightsPoints(JSON.parse(json)['sights']);
+                      
+                    });
+                }
+
+                getSightsPoins(map);
+            
+        },
+
+        getSightById (id) {
+            console.log(id);
+            fetch("/get/sight/by/id", 
+                {
+                    method: "POST",
+                    body: JSON.stringify({ 
+                        id : id,
+                    }),
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                        // Authorization: 'Bearer '+ localStorage.access_token ,
+                    }
+                    }
+                    ).then((response) => response.json()).then((json) => {
+                       
+                        // updateSightsPoints(JSON.parse(json)['sights']);
+                        console.log(json);
+                        this.selectedOnRouteEditMapSight = json
+                });
+                
         },
 
         logout () {
@@ -192,7 +354,12 @@ const App = {
             // window.location.pathname = 'login'; 
             window.location.reload();
         }
-    }
+    },
+    // watch: {
+    //     <>: function(){
+    //       console.log('any of these variables changed');
+    //     }
+    // }
 }
 
 
@@ -200,5 +367,7 @@ const App = {
 // app.mount("#app")
 
 // or
+// Vue.prototype.window = window
+// app.config.globalProperties.selectedOnRouteEditMapSight = 'selectedOnRouteEditMapSight';
 
 Vue.createApp(App).mount("#app")
